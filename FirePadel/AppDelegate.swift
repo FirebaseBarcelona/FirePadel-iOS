fd170273e1e423fadfc48a85f6d98edacd652610
Julian Oliver <julian.oliver@wallapop.com>
2016-09-08 19:14:13 +0200

    Project setup

---
+++ FirePadel/AppDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FIRApp.configure()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.3'
+inhibit_all_warnings!
+
+target 'FirePadel' do
+  use_frameworks!
+
+  pod 'Firebase'
+  pod 'RxSwift', :git => 'https://github.com/ReactiveX/RxSwift.git', :branch => 'develop'
+  pod 'RxCocoa', :git => 'https://github.com/ReactiveX/RxSwift.git', :branch => 'develop'
+end
